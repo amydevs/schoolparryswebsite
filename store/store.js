751754ae03c16729e6cf59f3601db2bf57cafcaa
jy1263 <50583248+jy1263@users.noreply.github.com>
2019-12-15 12:11:16 +1100

    f

---
+++ store/store.js
@@ -0,0 +1,85 @@
+// JavaScript Document
+function setCookie(name, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = '1=' + nameofitem + '; expires=' + d.toGMTString() + ';'; 
+}
+
+function setCookie1(cname, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem1").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = '2=' + nameofitem + '; expires=' + d.toGMTString() + ';';   
+}
+function setCookie2(cname, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem2").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = nameofitem +', '+ keycookie;  
+}
+function setCookie3(cname, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem3").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = nameofitem +', '+ keycookie; 
+}
+function setCookie4(cname, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem4").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = nameofitem +', '+ keycookie; 
+}
+function setCookie5(cname, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem5").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = nameofitem +', '+ keycookie; 
+}
+function setCookie6(cname, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem6").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = nameofitem +', '+ keycookie; 
+}
+function setCookie7(cname, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem7").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = nameofitem +', '+ keycookie; 
+}
+function setCookie8(cname, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem8").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = nameofitem +', '+ keycookie; 
+}
+function setCookie9(cname, cvalue, exdays) {
+	var keycookie = document.cookie;
+	var nameofitem = document.getElementById("nameofitem9").innerHTML;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = nameofitem +', '+ keycookie; 
+}
+
+	
+
